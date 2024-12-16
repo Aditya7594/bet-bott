7521c1868d2e08285b5ef873583a86a0379f5e32
Aditya Nayak <155938673+Aditya7594@users.noreply.github.com>
2024-12-16 08:32:06 +0530

    Update hilo.py

--- hilo.py
+++ hilo.py
@@ -1,7 +1,8 @@
 import random
 import re
 from pymongo import MongoClient
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.constants import ParseMode
 from telegram.ext import CallbackContext
 
 # MongoDB setup
