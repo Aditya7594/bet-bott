c571ba706bcfec5aa50460ee8490b56ee2e44841
Aditya Nayak <155938673+Aditya7594@users.noreply.github.com>
2025-03-20 09:05:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from claim import daily, random_claim, claim_credits, send_random_claim
 from bank import exchange, sell, store, withdraw, bank
 from hilo_game import hilo, hilo_click, hilo_cashout
 from cards import gacha, gacha, my_collection,view_card, card_pull
-from mines_game import Mines, mines_click, mines_cashout
+from mines_game import Mines, Mines_click, Mines_cashout
 OWNER_ID = 5667016949
 muted_users = set()
 last_interaction_time = {}
