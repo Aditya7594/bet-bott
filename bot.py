4d429155df69974386d0a5fd3e7f44ae4a1eebb7
Aditya Nayak <155938673+Aditya7594@users.noreply.github.com>
2025-03-26 19:56:40 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from token_1 import token
 
 
 from genshin_game import pull, bag, reward_primos, add_primos, leaderboard, handle_message, button, reset_bag_data, drop_primos, set_threshold, handle_artifact_button,send_artifact_reward
-from cricket import chat_cricket, join_cricket, toss_button, choose_button, play_button, update_game_interface, handle_wicket, end_innings, declare_winner, chat_message, watch_game
+from cricket import chat_cricket, handle_cricket_callback, handle_join_game, handle_watch_game, handle_toss, handle_choose, handle_play, handle_wicket, handle_end_innings, declare_winner, handle_cricket_message
 from minigame import dart, basketball, flip, dice, credits_leaderboard,football,help_command, start_command, roll,handle_flip_again
 from bdice import bdice
 from claim import daily, random_claim, claim_credits, send_random_claim
