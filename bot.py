1e66934bb48fecc304e6ea2177983ae19fabe1e6
Aditya Nayak <155938673+Aditya7594@users.noreply.github.com>
2025-05-04 11:07:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ from cricket import (
     setup_jobs,
 )
 from claim import get_claim_handlers, daily
-from wordhunt import register_handlers, games, timers
+from wordhunt import register_handlers, games
 from bank import bank, store, withdraw, add_credits, blacklist, unblacklist, auto_ban,scan_blacklist
 from mines_game import get_mines_handlers
 from hilo_game import get_hilo_handlers
