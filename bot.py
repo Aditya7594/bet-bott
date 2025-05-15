3656dc7f17ae31fff34fde4baefc857b0b757179
Aditya Nayak <155938673+Aditya7594@users.noreply.github.com>
2025-05-15 14:36:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
+from flask import Flask
+from threading import Thread
+
+# Create Flask app
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Shadow'
+
+# Function to run Flask
+def run_flask():
+    app.run(host="0.0.0.0", port=8000)
+
+# Start Flask server in background thread
+flask_thread = Thread(target=run_flask)
+flask_thread.start()
 import asyncio
 from pymongo import MongoClient
 import os
